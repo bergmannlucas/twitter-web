cdd406dbdb9aa7a010c2ff8f5fdc6f84a1b765ca
bergmannlucas <lucaasberg@gmail.com>
2019-01-16 11:34:43 -0200

    update baseURL to HTTPS

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const baseURL = 'http://twitter-backend-goweek.herokuapp.com';
+export const baseURL = 'https://twitter-backend-goweek.herokuapp.com';
 
 export const api = axios.create({
   baseURL: baseURL,
